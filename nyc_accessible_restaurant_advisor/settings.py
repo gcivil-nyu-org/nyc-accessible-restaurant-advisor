8535cdd68406dd2e83b929cecc7a37b4aa868af5
xiaodan-tang <shonna.x.tang@gmail.com>
2020-10-13 23:37:34 -0400

    Add Email Verification Function && Add Signup Page

--- nyc_accessible_restaurant_advisor/settings.py
+++ nyc_accessible_restaurant_advisor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,5 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nyc.accessible.rest@gmail.com'
+EMAIL_HOST_PASSWORD = '6063team408'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
 
 django_heroku.settings(locals())
