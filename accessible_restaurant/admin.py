016eff07e7de40892b4e29a8493fc3b68041a980
Ritukuklani <rtkuklani@gmail.com>
2020-10-27 14:21:57 -0400

    Build fixed

--- accessible_restaurant/admin.py
+++ accessible_restaurant/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User, User_Profile, Restaurant_Profile, Restaurant
 from import_export.admin import ImportExportModelAdmin
+
 # Register your models here.
 
 
@@ -9,6 +10,7 @@ admin.site.register(User_Profile)
 admin.site.register(Restaurant_Profile)
 # admin.site.register(Restaurant)
 
+
 @admin.register(Restaurant)
-class ViewAdmin (ImportExportModelAdmin):
+class ViewAdmin(ImportExportModelAdmin):
     pass
