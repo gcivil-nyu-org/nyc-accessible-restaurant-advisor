bcecc375708c0e6d8fa1664eb0f356e05b185e76
Ritukuklani <rtkuklani@gmail.com>
2020-11-03 11:55:19 -0500

    Comments removed

--- accessible_restaurant/urls.py
+++ accessible_restaurant/urls.py
@@ -22,16 +22,6 @@ urlpatterns = [
         ),
         name="password-reset",
     ),
-    path(
-        "accounts/password-reset/done/",
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="accounts/password_reset_done.html"
-        ),
-        name="password_reset_done",
-    ),
-    # path('password_reset_confirm/<uidb64>/<token>/',
-    #     auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
-    #     name='password_reset_confirm'),
     path("accounts/signup/", views.signup_view, name="signup"),
     path(
         "accounts/signup/usersignup/",
