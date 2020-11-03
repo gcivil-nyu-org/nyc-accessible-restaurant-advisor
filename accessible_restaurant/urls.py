d91e0050e73e6a28a69802256e54cdcb7fa0f6e6
Ritukuklani <rtkuklani@gmail.com>
2020-11-03 11:48:07 -0500

    Urls added

--- accessible_restaurant/urls.py
+++ accessible_restaurant/urls.py
@@ -15,6 +15,23 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="index.html"),
         name="logout",
     ),
+    path(
+        "accounts/password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html"
+        ),
+        name="password-reset",
+    ),
+    path(
+        "accounts/password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    # path('password_reset_confirm/<uidb64>/<token>/',
+    #     auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+    #     name='password_reset_confirm'),
     path("accounts/signup/", views.signup_view, name="signup"),
     path(
         "accounts/signup/usersignup/",
