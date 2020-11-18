3cc7ca695413a4e11ba226f92dfc4c500c14c5b3
Huanjin-Zhang <hz2169@nyu.edu>
2020-11-17 23:00:11 -0500

    add katie's test

--- accessible_restaurant/tests.py
+++ accessible_restaurant/tests.py
@@ -703,77 +703,108 @@ class FilterTest(TestCase):
             phone="12122670860",
             compliant=True,
             price="$",
-            category1="Pizza"
+            category1="Pizza",
         )
 
         self.filter_url = reverse("accessible_restaurant:browse", args=["0", "default"])
 
     def test_individual_filter_price(self):
 
-        response_filter_most_expensive = self.client.get(self.filter_url, {"price4": "$$$$"})
-        response_filter_less_expensive = self.client.get(self.filter_url, {"price3": "$$$"})
-        response_filter_least_expensive = self.client.get(self.filter_url, {"price1": "$"})
+        response_filter_most_expensive = self.client.get(
+            self.filter_url, {"price4": "$$$$"}
+        )
+        response_filter_less_expensive = self.client.get(
+            self.filter_url, {"price3": "$$$"}
+        )
+        response_filter_least_expensive = self.client.get(
+            self.filter_url, {"price1": "$"}
+        )
 
-        string_most_expensive = response_filter_most_expensive.content.decode(encoding='UTF-8')
-        string_less_expensive = response_filter_less_expensive.content.decode(encoding='UTF-8')
-        string_least_expensive = response_filter_least_expensive.content.decode(encoding='UTF-8')
+        string_most_expensive = response_filter_most_expensive.content.decode(
+            encoding="UTF-8"
+        )
+        string_less_expensive = response_filter_less_expensive.content.decode(
+            encoding="UTF-8"
+        )
+        string_least_expensive = response_filter_least_expensive.content.decode(
+            encoding="UTF-8"
+        )
 
         self.assertEqual(response_filter_most_expensive.status_code, 200)
         self.assertEqual(response_filter_less_expensive.status_code, 200)
         self.assertEqual(response_filter_least_expensive.status_code, 200)
 
-        self.assertIn("jkl1ukPtVM2UZqMLSJdWFw",string_most_expensive) # Greenwich Steakhouse
-        self.assertNotIn("zuD-iB7hV_dnf_JzBk_DCQ",string_most_expensive) # Juku
-        self.assertNotIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_most_expensive) # Joe's Pizza
+        self.assertIn(
+            "jkl1ukPtVM2UZqMLSJdWFw", string_most_expensive
+        )  # Greenwich Steakhouse
+        self.assertNotIn("zuD-iB7hV_dnf_JzBk_DCQ", string_most_expensive)  # Juku
+        self.assertNotIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_most_expensive)  # Joe's Pizza
 
-        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ",string_less_expensive) # Juku
-        self.assertNotIn("jkl1ukPtVM2UZqMLSJdWFw",string_less_expensive) # Greenwich Steakhouse
-        self.assertNotIn("4h4Tuuc56YPO6lWfZ1bdSQ",string_less_expensive) # Joe's Pizza
+        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ", string_less_expensive)  # Juku
+        self.assertNotIn(
+            "jkl1ukPtVM2UZqMLSJdWFw", string_less_expensive
+        )  # Greenwich Steakhouse
+        self.assertNotIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_less_expensive)  # Joe's Pizza
 
-        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ",string_least_expensive) # Joe's Pizza
-        self.assertNotIn("jkl1ukPtVM2UZqMLSJdWFw",string_least_expensive) # Greenwich Steakhouse
-        self.assertNotIn("zuD-iB7hV_dnf_JzBk_DCQ",string_least_expensive) # Juku
+        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_least_expensive)  # Joe's Pizza
+        self.assertNotIn(
+            "jkl1ukPtVM2UZqMLSJdWFw", string_least_expensive
+        )  # Greenwich Steakhouse
+        self.assertNotIn("zuD-iB7hV_dnf_JzBk_DCQ", string_least_expensive)  # Juku
 
     def test_group_filter_price(self):
 
-        response_filter_more_expensive = self.client.get(self.filter_url, {"price4": "$$$$","price3": "$$$"})
-        response_filter_less_expensive = self.client.get(self.filter_url, {"price3": "$$$", "price1": "$"})
-        response_filter_all = self.client.get(self.filter_url, {"price1": "$", "price3": "$$$", "price4": "$$$$"})
+        response_filter_more_expensive = self.client.get(
+            self.filter_url, {"price4": "$$$$", "price3": "$$$"}
+        )
+        response_filter_less_expensive = self.client.get(
+            self.filter_url, {"price3": "$$$", "price1": "$"}
+        )
+        response_filter_all = self.client.get(
+            self.filter_url, {"price1": "$", "price3": "$$$", "price4": "$$$$"}
+        )
 
-        string_more_expensive = response_filter_more_expensive.content.decode(encoding='UTF-8')
-        string_less_expensive = response_filter_less_expensive.content.decode(encoding='UTF-8')
-        string_all = response_filter_all.content.decode(encoding='UTF-8')
+        string_more_expensive = response_filter_more_expensive.content.decode(
+            encoding="UTF-8"
+        )
+        string_less_expensive = response_filter_less_expensive.content.decode(
+            encoding="UTF-8"
+        )
+        string_all = response_filter_all.content.decode(encoding="UTF-8")
 
         self.assertEqual(response_filter_more_expensive.status_code, 200)
         self.assertEqual(response_filter_less_expensive.status_code, 200)
         self.assertEqual(response_filter_all.status_code, 200)
 
-        self.assertIn("jkl1ukPtVM2UZqMLSJdWFw",string_more_expensive) # Greenwich Steakhouse
-        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ",string_more_expensive) # Juku
-        self.assertNotIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_more_expensive) # Joe's Pizza
+        self.assertIn(
+            "jkl1ukPtVM2UZqMLSJdWFw", string_more_expensive
+        )  # Greenwich Steakhouse
+        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ", string_more_expensive)  # Juku
+        self.assertNotIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_more_expensive)  # Joe's Pizza
 
-        self.assertNotIn("jkl1ukPtVM2UZqMLSJdWFw",string_less_expensive) # Greenwich Steakhouse
-        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ",string_less_expensive) # Juku
-        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_less_expensive) # Joe's Pizza
+        self.assertNotIn(
+            "jkl1ukPtVM2UZqMLSJdWFw", string_less_expensive
+        )  # Greenwich Steakhouse
+        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ", string_less_expensive)  # Juku
+        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_less_expensive)  # Joe's Pizza
 
-        self.assertIn("jkl1ukPtVM2UZqMLSJdWFw",string_all) # Greenwich Steakhouse
-        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ",string_all) # Juku
-        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_all) # Joe's Pizza
-
+        self.assertIn("jkl1ukPtVM2UZqMLSJdWFw", string_all)  # Greenwich Steakhouse
+        self.assertIn("zuD-iB7hV_dnf_JzBk_DCQ", string_all)  # Juku
+        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_all)  # Joe's Pizza
 
     def test_category_filter(self):
 
         response_filter_category = self.client.get(self.filter_url, {"Pizza": "Pizza"})
 
-        string_response = response_filter_category.content.decode(encoding='UTF-8')
+        string_response = response_filter_category.content.decode(encoding="UTF-8")
 
         self.assertEqual(response_filter_category.status_code, 200)
 
-        self.assertNotIn("jkl1ukPtVM2UZqMLSJdWFw",string_response) # Greenwich Steakhouse
-        self.assertNotIn("zuD-iB7hV_dnf_JzBk_DCQ",string_response) # Juku
-        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_response) # Joe's Pizza
-
-
+        self.assertNotIn(
+            "jkl1ukPtVM2UZqMLSJdWFw", string_response
+        )  # Greenwich Steakhouse
+        self.assertNotIn("zuD-iB7hV_dnf_JzBk_DCQ", string_response)  # Juku
+        self.assertIn("4h4Tuuc56YPO6lWfZ1bdSQ", string_response)  # Joe's Pizza
 
 
 class TestModels(TestCase):
